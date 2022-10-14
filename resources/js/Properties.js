3851db1c076809d258b5f10033f242e3e6e0eaf7
Кирдянов Виктор Евгеньевич <KirdianovVE@01050-1.ru>
2022-10-14 18:26:36 +0300

    [fix] Вернул удаленную константу цвета

--- resources/js/Properties.js
+++ resources/js/Properties.js
@@ -4,3 +4,4 @@ const headerColor = "#3ddb72";
 const sidePanelColor = "#dde7e8";
 const customSpinBoxButtonColor = "navy";
 const customSpinBoxButtonHoverColor = "#0707a4";
+const customCheckBoxCheckedItemColor = "#137ea8";
